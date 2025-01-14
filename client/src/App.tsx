64951a1cee240aa719db0d41085ab780c7b9aa43
Yeniffer8a <jennyov19@hotmail.com>
2025-01-14 18:18:52 -0500

    grid container for the dashboard and container for the predictions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,30 @@
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
-import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./scenes/navbar"
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "./scenes/preditions";
 
 function App() {
-  const theme =useMemo(()=>createTheme(themeSettings),[])
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
-<div className="app">
-  <BrowserRouter>
-  <ThemeProvider theme={theme}>
-    <CssBaseline/>
-    <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-    <Navbar />
-      <Routes>
-        <Route path="/"></Route>
-        <Route path="/predictions"></Route>
-      </Routes>
-    </Box>
-  </ThemeProvider>
-  </BrowserRouter>
-</div>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />}></Route>
+              <Route path="/predictions" element={<Predictions />}></Route>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/scenes/preditions/index.tsx
@@ -0,0 +1,14 @@
+import DashboardBox from "@/components/DashboardBox";
+
+const Predictions = () => {
+  return (
+    <DashboardBox
+      width="100%"
+      height="100%"
+      p="1rem"
+      overflow="hidden"
+    ></DashboardBox>
+  );
+};
+
+export default Predictions;
